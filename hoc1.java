d2cd3984903de3558651f529844d4cd2d1716004
c200563 <c200563@ccg.ac.jp>
2023-01-09 23:25:46 +0900

    lesoon3

---
+++ hoc1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class hoc1{
+    public static void main(String[] args) {
+        //System.out.println(" Nhap nam :");
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+       System.out.println("dien tich ="+a*b);
+       }
+    }
